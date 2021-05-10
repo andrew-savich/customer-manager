e03b1d4016627f604a02b9acef37dbd0ee745d3b
andrew-savich <savichandrew@Gmail.com>
2021-05-11 00:15:36 +0300

    Added Project Lombok

--- customer-manager/src/main/java/com/andrewsavich/customermanager/model/Customer.java
+++ customer-manager/src/main/java/com/andrewsavich/customermanager/model/Customer.java
@@ -5,7 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
 public class Customer {
 	
 	@Id
@@ -14,41 +19,4 @@ public class Customer {
 	private String name;
 	private String email;
 	private String address;
-	
-	
-	public Customer() {
-
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + "]";
-	}
-	
-	
 }
